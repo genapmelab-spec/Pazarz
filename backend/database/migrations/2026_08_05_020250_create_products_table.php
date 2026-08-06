4606ff37acc434f5f2a1818158f80430a1831cc0
genapmelab <genapmelab@gmail.com>
2026-08-06 22:44:59 +0700

    update product

--- backend/database/migrations/2026_08_05_020250_create_products_table.php
+++ backend/database/migrations/2026_08_05_020250_create_products_table.php
@@ -13,7 +13,31 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('seller_id')
+                ->constrained('sellers')
+                ->cascadeOnDelete();
+
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->restrictOnDelete();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+
+            $table->enum('status', [
+                'draft',
+                'active',
+                'inactive',
+                'archived',
+            ])->default('draft');
+
+            $table->decimal('base_price', 12, 2);
+            $table->decimal('rating_avg', 3, 2)->default(0);
+            $table->unsignedInteger('review_count')->default(0);
             $table->timestamps();
+            $table->index(['seller_id', 'status']);
+            $table->index(['category_id', 'status']);
         });
     }
 
